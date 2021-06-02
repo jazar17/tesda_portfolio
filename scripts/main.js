6d44bb254db4aa941a6bbe5f53c4e0f2e75a4b21
Jobert Azares <jobert.azares@gmail.com>
2021-06-02 19:53:52 +0800

    Adding contact

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,22 @@ $(document).ready(function () {
   let height = $(window).height();
   $(".sidenav").sidenav({ edge: "right" });
   $(".slider").slider({ height: height, indicators: false });
-  $('.materialboxed').materialbox();
+  $(".materialboxed").materialbox();
+
+  $(".nav-link").hover(
+    function () {
+      $(this).removeClass("underline_out").addClass("underline_in");
+    },
+    function () {
+      $(this).removeClass("underline_in").addClass("underline_out");
+    }
+  );
+
+  $('textarea#textarea1').characterCounter();
+
+  let url = $(location)
+    .attr("href")
+    .split("/")
+    .filter((part) => part);
+  $(`a[href="/${url[url.length - 1]}"]`).addClass("active");
 });
